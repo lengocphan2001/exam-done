b695593d8a416bbfed6f4785189924c472a23e5b
unknown <Administrator@MAY27.MAY01>
2023-07-22 19:35:12 +0700

    add layout

---
+++ resources/views/user/dashboard.blade.php
@@ -0,0 +1,351 @@
+@extends('user.layout')
+
+@section('content')
+    <div class="container mt-5">
+        <h1 class="text-center text-white">ĐỀ THI SÁT HẠCH BẰNG LÁI XE A1 MỚI NHẤT NĂM 2023</h1>
+
+        <div class="row col-15 gx-5">
+            <div class="col-md-5 col-sm-15 col-xs-15 card p-5">
+                <div class="panel panel-default hidden-sm hidden-xs">
+                    <div class="panel-body">
+                        <h2>
+                            <p style="text-align:center"><span
+                                    style="color: #0000ff; font-family: arial, helvetica, sans-serif;"><strong
+                                        class="text-success">PHẦN MỀM LUYỆN THI LÝ THUYẾT 200 CÂU A1</strong></span></p>
+                        </h2>
+                        <p style="text-align: center;"><img
+                                src="https://hoclaixemoto.com/image200/thi-bang-lai-xe-may-a1-online.jpg"></p>
+                        <span style="text-align: justify; font-size: 100%; font-family: arial, helvetica, sans-serif;">Phần
+                            mềm được phát triển dựa trên cấu trúc bộ đề thi sát hạch lý thuyết lái xe mô tô hạng A1 do Tổng
+                            Cục Đường Bộ Việt Nam quy định trong kỳ thi sát hạch chính thức.</span>
+                        <p style="text-align: justify;" class="text-success"><span
+                                style="font-family: arial, helvetica, sans-serif; font-size: 100%;">Để tập phần thi lý
+                                thuyết bằng lái xe A1 tốt nhất, các học viên có thể sử dụng trực tiếp những bộ đề thi này. Bởi
+                                chúng tôi đã tổng hợp đầy đủ 200 câu hỏi thi bằng lái xe máy A1 đã đánh dấu sẵn đáp án và
+                                câu hỏi điểm liệt.</span></p>
+                        
+                    </div>
+                </div>
+                
+            </div>
+            <div class="col-md-7 col-sm-15 col-xs-15 card p-5">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <h2 style="text-align:center; "><span style="color: #ff0000;"><strong class="text-success">BỘ ĐỀ THI
+                                    THỬ BẰNG LÁI XE MÁY A1</strong></span></h2>
+                        <div>
+                            <p style="text-align: justify;" class="text-primary">Cấu trúc bộ đề thi sát hạch giấy phép lái xe hạng A1 sẽ bao gồm
+                                <strong>25 câu hỏi</strong>, mỗi câu hỏi chỉ có <strong>duy nhất 1 đáp trả lời
+                                    đúng</strong>. Khác hẳn với bộ đề thi luật cũ là 2 đáp án. Mỗi đề thi chúng tôi sẽ bố
+                                trí từ <strong>2 - 4 câu hỏi điểm liệt</strong> để học viên có thể làm quen và ghi nhớ,
+                                tránh việc làm sai câu hỏi liệt.
+                            </p>
+                            <ul style="text-align: justify;">
+                                <li>Số lượng câu hỏi: <strong><span style="color: #ff0000;">25 Câu</span></strong>.</li>
+                                <li>Yêu cầu làm đúng <span style="color: #ff0000;"><strong>21/25 Câu</strong></span>.</li>
+                            </ul>
+                            <p style="text-align: justify;" class="text-danger"><strong>Lưu ý đặc biệt:</strong> <span
+                                     class="text-primary">Tuyệt đối không được làm sai câu hỏi điểm liệt, vì trong kỳ thi
+                                    thật nếu học viên làm sai "<strong class="text-danger">Câu Điểm Liệt</strong>" đồng nghĩa với việc
+                                    "<strong class="text-warning">KHÔNG ĐẠT</strong>" dù cho các câu khác trả lời đúng!</span></p>
+                            <div>
+                                <div style="margin-bottom:10px">
+                                    <strong style="font-size: 16px; color: red;">
+                                        Chọn đề thi để luyện:
+                                    </strong>
+                                </div>
+                                <div>
+                                    <strong style="font-size: 16px;">
+                                        <button type="submit" class="btn btn-success btn-thongtin" name="chondethi"
+                                            value="de1">Đề 1</button>
+                                        <button type="submit" class="btn btn-success btn-thongtin" name="chondethi"
+                                            value="de2">Đề 2</button>
+                                        <button type="submit" class="btn btn-success btn-thongtin" name="chondethi"
+                                            value="de3">Đề 3</button>
+                                        <button type="submit" class="btn btn-success btn-thongtin" name="chondethi"
+                                            value="de4">Đề 4</button>
+                                        <button type="submit" class="btn btn-success btn-thongtin" name="chondethi"
+                                            value="de5">Đề 5</button>
+                                        <button type="submit" class="btn btn-success btn-thongtin" name="chondethi"
+                                            value="de6">Đề 6</button>
+                                        <button type="submit" class="btn btn-success btn-thongtin" name="chondethi"
+                                            value="de7">Đề 7</button>
+                                        <button type="submit" class="btn btn-success btn-thongtin" name="chondethi"
+                                            value="de8">Đề 8</button>
+                                    </strong>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+<style>
+    [type=radio] {
+        display: none;
+    }
+
+    div.content img.hinhnho {
+        height: auto;
+
+    }
+
+    .thongtin {
+
+        width: 70%;
+        margin-left: 15%;
+        height: 50px;
+        background-color: #000;
+
+    }
+
+    .margincauhoi {
+        margin: 5px;
+    }
+
+    .nopdapan {
+        background-color: red;
+    }
+
+    .labelcauhoi {
+        /*min-width: 20px;
+ min-height: 20px;*/
+    }
+
+    body {
+        font-family: apple-system, BlinkMacSystemFont, sans-serif;
+        font-size: 17px;
+        line-height: 1.7;
+        color: #333;
+    }
+
+    .inner {
+        background: aliceblue;
+        border: 1px solid blue;
+        margin: 5px 0;
+    }
+
+    .fixed {
+        position: fixed;
+        top: 0;
+    }
+
+
+    .abcxyz {
+        padding: 10px;
+        background-color: #abc;
+    }
+
+    .example {
+        background-color: #fff;
+        margin: 0px;
+        height: auto;
+        border: 1px solid #000;
+    }
+
+
+    .pull-left {
+        width: 60%;
+    }
+
+    .nhomchondethi {
+        border: 1px solid #000;
+        margin-top: 10px;
+        height: auto;
+    }
+
+    .chondethi {
+        margin-top: 10px;
+        margin-bottom: 10px;
+        padding: 3px 10px 3px 10px;
+        background-color: #BDBDBD;
+        border: 1px solid #000;
+    }
+
+
+    .thithu {
+        margin: 20px;
+        padding: 3px 12px 3px 12px;
+        background-color: #BDBDBD;
+        border: 1px solid #000;
+    }
+
+    input.chondethi:hover {
+        background-color: #DF0174;
+    }
+
+    .btn-info {
+        width: 48px;
+        height: 48px;
+    }
+
+
+
+    table tr td.cau {
+        width: 20px;
+    }
+
+    table tr td.chon {
+        width: 30px;
+    }
+
+    input[type=checkbox] {}
+
+    /*troi test cot*/
+    .cot1 {
+        background-color: #BDBDBD;
+        border: 1px solid #000;
+        padding: 0px 5px 0px 5px;
+    }
+
+    .cot2 {
+        background-color: #81BEF7;
+        border: 1px solid #000;
+        padding: 0px 5px 0px 5px;
+    }
+
+    .bntchonde {
+        margin: 10px 0px 10px 0px;
+        background-color: #BDBDBD;
+        border: 1px solid #000;
+    }
+
+    /**************/
+    .btn-thongtin {
+        margin-bottom: 10px;
+    }
+
+    .btn-cauhoi.choose {
+        background-color: #FFC107;
+        border-color: #FFC107 !important;
+        color: #fff;
+    }
+
+    .btn-outline-dark {
+        color: #343a40 !important;
+        border-color: #343a40 !important;
+        background-color: #fff !important;
+    }
+
+    .btn-cauhoi {
+        margin: 0.15em;
+        margin-bottom: 0.3em;
+        font-size: 1.2em;
+        width: 2.7em;
+    }
+
+
+    .cautraloi {
+        border-style: ridge;
+        width: 90%;
+        margin-bottom: 0.7em;
+    }
+
+    .mauxanh {
+        font-weight: bold;
+        color: #000099;
+    }
+
+    .maudo {
+        color: red;
+    }
+
+    .ndcauhoi {
+        display: none;
+    }
+
+    input.question {
+        display: inline-block !important;
+        float: left;
+        margin-right: 10px;
+        cursor: pointer;
+    }
+
+    input.answer {
+        display: inline-block !important;
+        float: left;
+        margin-right: 10px;
+        cursor: pointer;
+    }
+
+    .text-number {
+        margin: 0 !important;
+    }
+
+    .btn-primary {
+        border-radius: 10.5px;
+        font-size: 16px;
+        background-color: #157eef;
+        border-color: #157eef;
+    }
+
+    .topnav {
+        overflow: hidden;
+        background-color: #34a853;
+        margin: 20px 0 10px;
+        border-radius: 0 15px;
+    }
+
+    .topnav a {
+        float: left;
+        display: block;
+        color: #fff;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        font-size: 17px;
+    }
+
+    .topnav a:hover {
+        background-color: #ffc902;
+        color: black;
+    }
+
+    .topnav a.active {
+        background-color: #ffc902;
+        color: #fff;
+    }
+
+    .topnav .icon {
+        display: none;
+    }
+
+    .panel-body-2 {
+        padding: 15px;
+        background-color: #104098;
+        color: #fff;
+    }
+
+    @media screen and (max-width: 600px) {
+        .topnav a:not(:first-child) {
+            display: none;
+        }
+
+        .topnav a.icon {
+            float: right;
+            display: block;
+        }
+    }
+
+    @media screen and (max-width: 600px) {
+        .topnav.responsive {
+            position: relative;
+        }
+
+        .topnav.responsive .icon {
+            position: absolute;
+            right: 0;
+            top: 0;
+        }
+
+        .topnav.responsive a {
+            float: none;
+            display: block;
+            text-align: left;
+        }
+    }
+</style>
--- resources/views/user/layout.blade.php
+++ resources/views/user/layout.blade.php
@@ -4,57 +4,43 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link href="{{ asset('https://fonts.googleapis.com/css?family=Open+Sans:300,400,700') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('user/fonts/icomoon/style.css') }}">
     <link rel="stylesheet" href="{{ asset('user/css/owl.carousel.min.css') }}">
     <link rel="stylesheet" href="{{ asset('user/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('user/css/style.css') }}">
-    <title></title>
+    <title>Trang chủ</title>
 </head>
 
-<body>
-    <div class="site-mobile-menu">
-        <div class="site-mobile-menu-header">
-            <div class="site-mobile-menu-close mt-3">
-                <span class="icon-close2 js-menu-toggle"></span>
-            </div>
-        </div>
-        <div class="site-mobile-menu-body"></div>
-    </div>
+<body class="text-black">
+
 
     <header class="site-navbar" role="banner">
-
-        <div class="container">
+        <div class="container-fluid pl-5">
             <div class="row align-items-center">
-
-                <div class="col-11 col-xl-2">
-                    <h1 class="mb-0 site-logo"><a href="index.html" class="text-white mb-0">Tên web</a></h1>
+                <div class="col-xl-2">
+                    <h1 class="mb-0 site-logo"><a href="{{ asset('dashboard') }}" class="text-white mb-0">Tên web</a></h1>
                 </div>
-                <div class="col-12 col-md-10 d-none d-xl-block">
+                <div class="col-md-9 d-none d-xl-block">
                     <nav class="site-navigation position-relative text-right" role="navigation">
-
                         <ul class="site-menu js-clone-nav mr-auto d-none d-lg-block">
-                            <li class="active"><a href="index.html"><span>Trang chủ</span></a></li>
+                            <li class="active"><a href="{{ asset('dashboard') }}"><span>Trang chủ</span></a></li>
 
-                            <li><a href="listings.html"><span>Thi thử A1</span></a></li>
-                            <li><a href="about.html"><span>Thi 20 câu điểm liệt</span></a></li>
-                            <li><a href="blog.html"><span>Lý thuyết</span></a></li>
+                            <li><a href="{{ asset('exam') }}"><span>Thi thử A1</span></a></li>
+                            <li><a href="{{ asset('about.html') }}"><span>Thi 20 câu điểm liệt</span></a></li>
+                            <li><a href="{{ asset('blog.html') }}"><span>Lý thuyết</span></a></li>
                             <li class="has-children">
                                 @if (Auth::user() && Auth::user()->isActive)
-                                
-                                    <a href=""><span>{{ Auth::user()->name }}</span></a>
+                                    <a href="{{ asset('#') }}"><span>{{ Auth::user()->name }}</span></a>
                                     <ul class="dropdown arrow-top">
-                                        <li><a href="{{ route('register') }}">Trang cá nhân</a></li>
-                                        <li><a href="{{ route('logout') }}">Đăng xuất</a></li>
-                                        {{-- <li><a href="#">Menu Two</a></li>
-                                    <li><a href="#">Menu Three</a></li> --}}
+                                        <li><a href="{{ asset('register') }}">Trang cá nhân</a></li>
+                                        <li><a href="{{ asset('logout') }}">Đăng xuất</a></li>
+                                        <li><a href="{{ asset('#') }}">Lịch sử thi</a></li>
                                     </ul>
                                 @else
-                                    <a href="{{ route('login') }}"><span>Đăng nhập</span></a>
+                                    <a href="{{ asset('login') }}"><span>Đăng nhập</span></a>
                                     <ul class="dropdown arrow-top">
-                                        <li><a href="{{ route('register') }}">Đăng ký</a></li>
-                                        {{-- <li><a href="#">Menu Two</a></li>
-                                    <li><a href="#">Menu Three</a></li> --}}
+                                        <li><a href="{{ asset('register') }}">Đăng ký</a></li>
                                     </ul>
                                 @endif
 
@@ -65,23 +51,28 @@
 
 
                 <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;"><a
-                        href="#" class="site-menu-toggle js-menu-toggle text-white"><span
+                        href="{{ asset('#') }}" class="site-menu-toggle js-menu-toggle text-white"><span
                             class="icon-menu h3"></span></a></div>
 
             </div>
 
         </div>
+        <div class="content">
+            @yield('content')
         </div>
 
     </header>
     <div class="hero" style="background-image: url('user/images/hero_1.jpg');">
-        @yield('content')
+        
     </div>
+    
+    
     <script src="{{ asset('user/js/jquery-3.3.1.min.js') }}"></script>
     <script src="{{ asset('user/js/popper.min.js') }}"></script>
     <script src="{{ asset('user/js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('user/js/jquery.sticky.js') }}"></script>
     <script src="{{ asset('user/js/main.js') }}"></script>
+
 </body>
 
 </html>
